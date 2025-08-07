20ec08f22f9a345f8970ead2bb4fe6f0c2f3e70e
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-07 11:48:40 +0200

    feat(api): Création d'une route pour l'accès aux salles depuis l'URL

--- projet-salles/index.js
+++ projet-salles/index.js
@@ -1,14 +1,12 @@
-const express = require("express");
 const fetchICSData = require("./server.js");
+const express = require("express");
+const path = require("path");
+const ICS_URLS = require("./public/resources/scripts/icsURL.js").ICS_URLS;
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
-
-app.listen(PORT, () => {
-  console.log(`✅ http://localhost:${PORT}`);
-});
 
 // Récuperation de tous les événements d'une salle spécifique
 app.get("/api/calendar/:id", async (req, res) => {
@@ -21,4 +19,18 @@ app.get("/api/calendar/:id", async (req, res) => {
     console.error("Erreur lors de la récupération des données", error.message);
     res.status(500).json({ error: error.message });
   }
+});
+
+app.get("/:roomId", (req, res) => {
+  const roomId = req.params.roomId;
+
+  if (!ICS_URLS[roomId]) {
+    return res.status(404).send("Erreur 404 : Salle inconnue");
+  }
+
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ http://localhost:${PORT}`);
 });
