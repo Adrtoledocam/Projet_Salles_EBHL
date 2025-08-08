26eb145b3a5dc467306b42b2d4bc469f3716de15
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-08 15:00:26 +0200

    feat(api): Modification d'une route existante pour la récupération de données sur la base de données

--- projet-salles/routes/api.js
+++ projet-salles/routes/api.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const fetchICSData = require("../server.js");
+const { fetchICSData, fetchDBData } = require("../server.js");
+const db = require("../config/db");
 
 // Récupération de tous les événements d'une salle spécifique
-router.get("/calendar/:site/:id", async (req, res) => {
+router.get("/calendar/:site/:room", async (req, res) => {
   try {
-    const { site, id } = req.params;
-    const events = await fetchICSData(site, id);
-    res.json(events.slice(0, 4));
+    const { site, room } = req.params;
+
+    // Récupération des événements pour PGA
+    if (site === "pga") {
+      const ICS_events = await fetchICSData(room);
+      res.json(ICS_events);
+    }
+    // Récupération des événements pour EBHL ou CDG
+    else if (site === "ebhl" || site === "cdg") {
+      const [DB_events] = await db.query(
+        "SELECT * FROM t_room WHERE romRoom = ?",
+        [room]
+      );
+      const filteredDB_events = await fetchDBData(DB_events);
+      res.json(filteredDB_events);
+    }
   } catch (error) {
     console.error("Erreur lors de la récupération des données", error.message);
     res.status(500).json({ error: error.message });
