363108de034950ded5470d22337cbe437bdb403d
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-08 15:27:00 +0200

    chore(file): Modification du prénom du fichier pour les URLS et nom de salles

---
+++ projet-salles/public/controllers/roomsData.js
@@ -0,0 +1,39 @@
+export const ICS_URLS = {
+  // Salles - Pôle Grand Âge
+  "Salle_PGA_Beau-Site_Colloque": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/726bb31e016a47dbbb8e399865c4a1d9@eben-hezer.ch/14454d8d84e741e98d21a203e8b339103993903089613600432/calendar.ics",
+    site: "pga",
+  },
+  "Salle_PGA_Beau-Site_Espace-Felix": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/a13f7558b5044127bf913ce2b5c23fa3@eben-hezer.ch/d4c79109624649199bc2892fff526b745409380396816643899/calendar.ics",
+    site: "pga",
+  },
+  "Salle_PGA_Beau-Site_Quart-DHeure-Vaudois": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/86fa51c8269f4906871ff9c78da70d42@eben-hezer.ch/94a4200763ef454da2ba6cf198484cfd12839416324990970364/calendar.ics",
+    site: "pga",
+  },
+  "Salle_PGA_Beau-Site_Veranda": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/ba0b45356d3f40ebbc063763fed13bf5@eben-hezer.ch/f49fe575d7664f56ad96f59a072ca79b2966595426940186045/calendar.ics",
+    site: "pga",
+  },
+  "Salle_PGA_Beau-Site_Vignerons": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/b6287f6654f04e5883b10befc62dc7f0@eben-hezer.ch/85753b96bdf44ff2a4062521f830a1be17337712867828642393/calendar.ics",
+    site: "pga",
+  },
+  Salle_PGA_Bonnettes_Colloque: {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/b838e7d766894afdaf7cea4e00b1a55b@eben-hezer.ch/b5dadfa0fc5041479a80dbdbf5b71ddf11107132211051048243/calendar.ics",
+    site: "pga",
+  },
+  "Salle_PGA_Coteau-Muraz_Colloque": {
+    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/21836e9a6aa0427095f45c47fda2aa4b@eben-hezer.ch/799e2cfed9554caca855c5f5a8737d852073725888054266005/calendar.ics",
+    site: "pga",
+  },
+  // Salles - Cité du Genévrier
+  Test_CDG: {
+    site: "cdg",
+  },
+  // Salles - Eben-Hézer Lausanne
+  Test_EBHL: {
+    site: "ebhl",
+  },
+};
