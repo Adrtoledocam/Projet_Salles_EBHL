01178f274596f75e998a30701b3e33b948865b7c
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-08 09:54:06 +0200

    feat(routes): Séparation des routes dans deux autres fichiers (api et frontend)

--- projet-salles/index.js
+++ projet-salles/index.js
@@ -1,35 +1,17 @@
-const fetchICSData = require("./server.js");
 const express = require("express");
-const path = require("path");
-const ICS_URLS = require("./public/resources/scripts/icsURL.js").ICS_URLS;
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static("public"));
 
-// Récuperation de tous les événements d'une salle spécifique
-app.get("/api/calendar/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const events = await fetchICSData(id);
+// Import des routes
+const apiRoutes = require("./routes/api");
+const frontendRoutes = require("./routes/frontend");
 
-    res.json(events.slice(0, 4));
-  } catch (error) {
-    console.error("Erreur lors de la récupération des données", error.message);
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.get("/:roomId", (req, res) => {
-  const roomId = req.params.roomId;
-
-  if (!ICS_URLS[roomId]) {
-    return res.status(404).send("Erreur 404 : Salle inconnue");
-  }
-
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Utilisation des routes
+app.use("/api", apiRoutes);
+app.use("/", frontendRoutes);
 
 app.listen(PORT, () => {
   console.log(`✅ http://localhost:${PORT}`);
