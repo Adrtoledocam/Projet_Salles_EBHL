18c6e6d2c8cf55c5341af584e1d9e8a6a2a21ba0
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-19 14:30:57 +0200

    fix(db): Création d'une fonctione afin de permettre de choisir la db souhaitée (ebhl/cdg)

--- projet-salles/config/db.js
+++ projet-salles/config/db.js
@@ -1,14 +1,17 @@
 const mysql = require("mysql2");
 
 // Informations de connexion à la base de données de staging
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "db_test",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+function setDbConfig(host, database) {
+  const pool = mysql.createPool({
+    host: host,
+    user: "root",
+    password: "root",
+    database: database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+  return pool.promise();
+}
 
-module.exports = pool.promise();
+module.exports = setDbConfig;
