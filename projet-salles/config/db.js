86afd4397e1b360094f46290761f2ac0f110aa38
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-25 11:55:37 +0200

    fix(env): Modification des noms de variables d'env

--- projet-salles/config/db.js
+++ projet-salles/config/db.js
@@ -1,14 +1,15 @@
 const mysql = require("mysql2");
-const env = require("dotenv").config();
+
+require("dotenv").config();
 
 // Informations de connexion à la base de données de staging
 function setDbConfig(host) {
   const pool = mysql.createPool({
     host: host,
-    user: env.USER,
-    password: env.PWD,
-    port: env.PORT_DB,
-    database: env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
@@ -17,7 +18,7 @@ function setDbConfig(host) {
   return pool.promise();
 }
 
-const db_ebhl = setDbConfig(env.EB_HOST);
-const db_cdg = setDbConfig(env.CDG_HOST);
+const db_ebhl = setDbConfig(process.env.EB_HOST);
+const db_cdg = setDbConfig(process.env.CDG_HOST);
 
 module.exports = { db_ebhl, db_cdg };
