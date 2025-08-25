60849e784669e4112d00a864045b9187f2950634
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-08-25 10:35:16 +0200

    fix(ics): Modification du lien ICS afin qu'il ne soit pas accessible depuis l'interne uniquement (mailpersonnel => exchange)

--- projet-salles/public/controllers/roomsData.js
+++ projet-salles/public/controllers/roomsData.js
@@ -1,13 +1,13 @@
 export const ROOMS_DATA = {
   // <--- LAUSANNE EAU --->
   Salle_Victoria: {
-    url: "https://mailpersonnel.eben-hezer.ch/OWA/calendar/9e3ef3231e6d44898aa2aa0943be4f26@eben-hezer.ch/f0e985e4302d42098e368dd8def1e47b9444836960257549334/calendar.ics",
+    url: "https://exchange.eben-hezer.ch/OWA/calendar/9e3ef3231e6d44898aa2aa0943be4f26@eben-hezer.ch/f0e985e4302d42098e368dd8def1e47b9444836960257549334/calendar.ics",
     site: "ebhl",
   },
 
   // <--- LAUSANNE MDC --->
   Salle_des_Pas_Perdus: {
-    url: "https://mailpersonnel.eben-hezer.ch/OWA/calendar/03037050045f4f77b999183c4d1e855e@eben-hezer.ch/19da1c99d5b84633bf9e3dfb2374953d1568427109262387736/calendar.ics",
+    url: "https://exchange.eben-hezer.ch/OWA/calendar/03037050045f4f77b999183c4d1e855e@eben-hezer.ch/19da1c99d5b84633bf9e3dfb2374953d1568427109262387736/calendar.ics",
     site: "ebhl",
   },
   Salle_Julie_Hofmann: {
@@ -37,11 +37,11 @@ export const ROOMS_DATA = {
     salle: "Tente extérieure Musardière",
   },
   "Bureau_Aumoniers-MJH": {
-    url: "https://mailpersonnel.eben-hezer.ch/OWA/calendar/26be97df56d24d3f9fd91bd59f69c69a@eben-hezer.ch/671876874a304de2aabaa7dad2fd120814811877050155374535/calendar.ics",
+    url: "https://exchange.eben-hezer.ch/OWA/calendar/26be97df56d24d3f9fd91bd59f69c69a@eben-hezer.ch/671876874a304de2aabaa7dad2fd120814811877050155374535/calendar.ics",
     site: "ebhl",
   },
   "Bureau_CDL-MJH": {
-    url: "https://mailpersonnel.eben-hezer.ch/OWA/calendar/e01c7313480745869d5e7e5f10335062@eben-hezer.ch/92dee8ff5a054cb9bd17a7fff5bee4fc11271071637650241910/calendar.ics",
+    url: "https://exchange.eben-hezer.ch/OWA/calendar/e01c7313480745869d5e7e5f10335062@eben-hezer.ch/92dee8ff5a054cb9bd17a7fff5bee4fc11271071637650241910/calendar.ics",
     site: "ebhl",
   },
 
@@ -71,7 +71,7 @@ export const ROOMS_DATA = {
     salle: "Salon",
   },
 
-  // Salles - Cité du Genévrier
+  // <--- CITE DU GENEVRIER --->
   Exploration_sensorielle_grande: {
     site: "cdg",
     salle: "Exploration sensorielle - grande salle",
@@ -127,35 +127,35 @@ export const ROOMS_DATA = {
 
   // <--- PGA BEAU SITE --->
   "Salle_PGA_Beau-Site_Vignerons": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/b6287f6654f04e5883b10befc62dc7f0@eben-hezer.ch/85753b96bdf44ff2a4062521f830a1be17337712867828642393/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/b6287f6654f04e5883b10befc62dc7f0@eben-hezer.ch/85753b96bdf44ff2a4062521f830a1be17337712867828642393/calendar.ics",
     site: "pga",
   },
   "Salle_PGA_Beau-Site_Colloque": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/726bb31e016a47dbbb8e399865c4a1d9@eben-hezer.ch/14454d8d84e741e98d21a203e8b339103993903089613600432/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/726bb31e016a47dbbb8e399865c4a1d9@eben-hezer.ch/14454d8d84e741e98d21a203e8b339103993903089613600432/calendar.ics",
     site: "pga",
   },
   "Salle_PGA_Beau-Site_Espace-Felix": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/a13f7558b5044127bf913ce2b5c23fa3@eben-hezer.ch/d4c79109624649199bc2892fff526b745409380396816643899/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/a13f7558b5044127bf913ce2b5c23fa3@eben-hezer.ch/d4c79109624649199bc2892fff526b745409380396816643899/calendar.ics",
     site: "pga",
   },
   "Salle_PGA_Beau-Site_Quart-DHeure-Vaudois": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/86fa51c8269f4906871ff9c78da70d42@eben-hezer.ch/94a4200763ef454da2ba6cf198484cfd12839416324990970364/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/86fa51c8269f4906871ff9c78da70d42@eben-hezer.ch/94a4200763ef454da2ba6cf198484cfd12839416324990970364/calendar.ics",
     site: "pga",
   },
   "Salle_PGA_Beau-Site_Veranda": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/ba0b45356d3f40ebbc063763fed13bf5@eben-hezer.ch/f49fe575d7664f56ad96f59a072ca79b2966595426940186045/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/ba0b45356d3f40ebbc063763fed13bf5@eben-hezer.ch/f49fe575d7664f56ad96f59a072ca79b2966595426940186045/calendar.ics",
     site: "pga",
   },
 
   // <--- PGA BONNETTES --->
   Salle_PGA_Bonnettes_Colloque: {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/b838e7d766894afdaf7cea4e00b1a55b@eben-hezer.ch/b5dadfa0fc5041479a80dbdbf5b71ddf11107132211051048243/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/b838e7d766894afdaf7cea4e00b1a55b@eben-hezer.ch/b5dadfa0fc5041479a80dbdbf5b71ddf11107132211051048243/calendar.ics",
     site: "pga",
   },
 
   // <--- PGA COTEAU-MURAZ --->
   "Salle_PGA_Coteau-Muraz_Colloque": {
-    url: "http://mailpersonnel.eben-hezer.ch/owa/calendar/21836e9a6aa0427095f45c47fda2aa4b@eben-hezer.ch/799e2cfed9554caca855c5f5a8737d852073725888054266005/calendar.ics",
+    url: "http://exchange.eben-hezer.ch/owa/calendar/21836e9a6aa0427095f45c47fda2aa4b@eben-hezer.ch/799e2cfed9554caca855c5f5a8737d852073725888054266005/calendar.ics",
     site: "pga",
   },
 };
