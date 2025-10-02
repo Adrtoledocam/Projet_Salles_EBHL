47e271b8b3636e7eff162905a67d388baf75af7b
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2025-10-02 12:58:39 +0200

    fix(fullscreen): Désactivation temporaire

--- public/resources/scripts/main.js
+++ public/resources/scripts/main.js
@@ -6,8 +6,6 @@ const selectedRoom = document.getElementById("calendar-select"); // Sélecteur d
 const roomStatus = document.getElementById("status"); // État de la salle (libre ou occupée)
 const clock = document.getElementById("clock");
 
-window.open("index.html", "fullscreen=yes");
-
 setInterval(() => {
   let date = new Date();
   clock.innerHTML = date.toLocaleTimeString("fr-FR", {
